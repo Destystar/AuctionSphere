e4c51981807762acd97feed5dac802f84c78c701
Destystar <95242511+Destystar@users.noreply.github.com>
2023-12-11 22:53:08 +0000

    fixed navbar and css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,10 @@
 
 <div>
     <nav class="bg-indigo-900 text-gray-300 text-lg font-bold h-9">
-        <ul class="flex h-full list-none w-full justify-evenly items-center">
-            <li>
-                <a href="/" class="block text-center">Home</a>
-            </li>
+        <ul class="flex h-full list-none w-full justify-between items-center px-4">
+            <h1>
+                <a href="/"><img class="h-9 " src={mainIcon} alt="Go to the HomePage"></a>
+            </h1>
             <li>
                 <a href="/dashboard" class="block text-center">Dashboard</a>
             </li>
@@ -18,6 +18,7 @@
             <li>
                 <a href="/discover" class="block text-center">Search</a>
             </li>
+            <li></li>
         </ul>
     </nav>
     <slot></slot>
