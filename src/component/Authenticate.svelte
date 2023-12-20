74fd7577750f772ba73cb9ffde79c13998c930a3
Destystar <95242511+Destystar@users.noreply.github.com>
2023-12-20 00:03:16 +0000

    error codes edited and added password validation

--- src/component/Authenticate.svelte
+++ src/component/Authenticate.svelte
@@ -5,15 +5,27 @@
     import { v4 as uuidv4 } from "uuid";
     import { doc, setDoc, collection } from "firebase/firestore";
 
+    // input variables 
     let email = "";
     let password = "";
     let confirmPassword = "";
+
+    // error variables 
     let errorAuth = false;
     let errorLack = false;
     let errorMatch = false;
-    let error = false;
+    let errorValid = false;
+
+    // status variables 
     let register = false;
     let authenticating = false;
+
+    // password validation variables 
+    let isLengthValid = false;
+    let isLowercaseValid = false;
+    let isUppercaseValid = false;
+    let isNumberValid = false;
+    let isSpecialCharacterValid = false;
 
 
     function createKey(){
@@ -28,6 +40,36 @@
     function encrypt(input, key) {
         return CryptoJS.AES.encrypt(input, key).toString();
     }
+
+    // @ts-ignore
+    function handlePasswordInput(event) {
+        const value = event.target.value;
+        updateRequirement('length', value.length >= 8);
+        updateRequirement('lowercase', /[a-z]/.test(value));
+        updateRequirement('uppercase', /[A-Z]/.test(value));
+        updateRequirement('number', /\d/.test(value));
+        updateRequirement('characters', /[^\w\d]/.test(value));
+
+    }
+
+    // @ts-ignore
+    function updateRequirement(id, isValid) {
+        if (id === 'length') {
+            isLengthValid = isValid;
+        } else if (id === 'lowercase') {
+            isLowercaseValid = isValid;
+        } else if (id === 'uppercase') {
+            isUppercaseValid = isValid;
+        } else if (id === 'number') {
+            isNumberValid = isValid;
+        } else if (id === 'characters') {
+            isSpecialCharacterValid = isValid;
+        }
+    }
+
+    function checkValid() {
+    return isLengthValid && isLowercaseValid && isUppercaseValid && isNumberValid && isSpecialCharacterValid;
+  }
 
     /**
      * @param {any} input
@@ -51,22 +93,28 @@
         errorAuth = false;
         errorLack = false;
         errorMatch = false;
+        errorValid = false;
 
         try {
 
             if (!register) {
-                if ( password != "" || email != "" ) {
+                // sign in execution
+                if ( password !== "" || email !== "" ) {
                     await authHandlers.login(email, password);
                 } else {
                     errorAuth = true;
                     errorLack = true;
                 }
             } else {
-                if (password === confirmPassword) {
+                // register execution
+                if (password === confirmPassword && checkValid()) {
                     await authHandlers.signup(email, password);
                 } else if (password === "" || confirmPassword === "" || email === "") {
                     errorAuth = true;
                     errorLack = true;
+                } else if (!checkValid()) {
+                    errorAuth = true;
+                    errorValid = true;
                 }  else {
                     errorAuth = true;
                     errorMatch = true;
@@ -91,9 +139,12 @@
     <!-- svelte-ignore a11y-no-static-element-interactions -->
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 flex flex-col gap-y-3 max-w-md w-full">
         <h1 class="text-3xl font-roboto font-bold text-gray-800 mb-4 text-center">{register ? "Register" : "Login"}</h1>
+        <!-- displays errors-->
         {#if errorAuth}
             {#if errorMatch}
                 <p class="flex justify-center text-red-700 ">Your passwords don't match</p>
+            {:else if errorValid}
+                <p class="flex justify-center text-red-700 ">Your password doesn't meet the minimum requirements</p>
             {:else}
                 <p class="flex justify-center text-red-700 ">{ errorLack ? "You haven't entered all of the required information" : "Please check the email or password you have entered" }</p>
             {/if}
@@ -109,9 +160,34 @@
         <div class="flex flex-col">
             <label class="text-gray-700 text-sm font-bold mb-2 flex items-center">
                 <span class="w-3/12">Password</span>
-                <input bind:value={password} class="shadow appearance-none border rounded py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline flex-1 ml-2" id="password" type="password" placeholder="Enter your password">
+                <input bind:value={password} on:input={handlePasswordInput} class="shadow appearance-none border rounded py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline flex-1 ml-2" id="password" type="password" placeholder="Enter your password">
             </label>
         </div>
+        <!-- password requirements check -->
+        {#if register}
+        <div class="flex flex-wrap items-center">
+            <p class="text-sm flex-1 min-w-max my-2 mx-0 {isLengthValid ? 'text-green-500' : 'text-red-500'}" id="length">
+                <span class="inline-block w-4 h-4 mr-1.5 rounded-full {isLengthValid ? 'bg-green-500' : 'bg-red-500'}"></span>
+                Min. 8 characters
+            </p>
+            <p class="text-sm flex-1 min-w-max my-2 mx-0 {isLowercaseValid ? 'text-green-500' : 'text-red-500'}" id="lowercase">
+                <span class="inline-block w-4 h-4 mr-1.5 rounded-full {isLowercaseValid ? 'bg-green-500' : 'bg-red-500'}"></span>
+                Include lowercase letter
+            </p>
+            <p class="text-sm flex-1 min-w-max my-2 mx-0 {isUppercaseValid ? 'text-green-500' : 'text-red-500'}" id="uppercase">
+                <span class="inline-block w-4 h-4 mr-1.5 rounded-full {isUppercaseValid ? 'bg-green-500' : 'bg-red-500'}"></span>
+                Include uppercase letter
+            </p>
+            <p class="text-sm flex-1 min-w-max my-2 mx-0 {isNumberValid ? 'text-green-500' : 'text-red-500'}" id="number">
+                <span class="inline-block w-4 h-4 mr-1.5 rounded-full {isNumberValid ? 'bg-green-500' : 'bg-red-500'}"></span>
+                Include number
+            </p>
+            <p class="text-sm flex-1 min-w-max my-2 mx-0 {isSpecialCharacterValid ? 'text-green-500' : 'text-red-500'}" id="characters">
+                <span class="inline-block w-4 h-4 mr-1.5 rounded-full {isSpecialCharacterValid ? 'bg-green-500' : 'bg-red-500'}"></span>
+                Include a special character
+            </p>
+        </div>        
+        {/if}
         <!-- Confirm Password -->
         {#if register}
             <div class="flex flex-col">
