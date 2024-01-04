a0b9c563dd07bc5676e36783e1f7a36a779dba85
Destystar <95242511+Destystar@users.noreply.github.com>
2024-01-04 00:34:59 +0000

    search pretty much done need to add bidding

--- src/routes/main/search/+page.svelte
+++ src/routes/main/search/+page.svelte
@@ -1,210 +1,239 @@
 <script>
-    //#region imports
-    import { onMount } from 'svelte';
-    import { collection, query, where, orderBy, limit, getDocs } from "firebase/firestore";
-    import { db, storage } from "$lib/firebase/firebase";
-    import { getDownloadURL, ref } from "firebase/storage";
-    import { eng } from 'stopword';
-  //#endregion
-  
-    let searchQuery = '';
-    /**
-     * @type {string | any[]}
-     */
-    let searchResults = [];
-    const itemsPerPage = 10;
-    let currentPage = 1;
-    let category = "Any";
+// @ts-nocheck
 
-    const stopwords = eng;
+  import { onMount } from 'svelte';
+  import { collection, query, where, orderBy, limit, getDocs } from "firebase/firestore";
+  import { db, storage } from "$lib/firebase/firebase";
+  import { getDownloadURL, ref } from "firebase/storage";
+  import { eng } from 'stopword';
+  import { writable } from 'svelte/store';
 
-    /**
-     * @param {string | undefined} imagePath
-     */
-    async function getImageUrl(imagePath) {
-      const imageRef = ref(storage, imagePath);
-      const imageUrl = await getDownloadURL(imageRef);
-      return imageUrl;
+  let searchQuery = '';
+  let searchResults = [];
+  const itemsPerPage = 10;
+  let currentPage = 1;
+  let category = "Any";
+  let displayCurrency = "GBP";
+  let timers = writable({});
+  const stopwords = eng;
+  let currencySymbol = "£";
+
+  async function getImageUrl(imagePath) {
+    const imageRef = ref(storage, imagePath);
+    const imageUrl = await getDownloadURL(imageRef);
+    return imageUrl;
+  }
+
+  function mergeSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
     }
 
-    //#region mergesort algorithm
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
 
-    // @ts-ignore
-    function mergeSort(arr) {
-      if (arr.length <= 1) {
-        return arr;
-      }
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
 
-      const mid = Math.floor(arr.length / 2);
-      // @ts-ignore
-      const left = mergeSort(arr.slice(0, mid));
-      // @ts-ignore
-      const right = mergeSort(arr.slice(mid));
-
-      let result = [];
-      let leftIndex = 0;
-      let rightIndex = 0;
-
-      while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex].title < right[rightIndex].title) {
-          result.push(left[leftIndex]);
-          leftIndex++;
-        } else {
-          result.push(right[rightIndex]);
-          rightIndex++;
-        }
-      }
-
-      return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-    }
-    //#endregion
-
-    /**
-     * @param {string} str
-     * @param {string | any[]} stopwords
-     */
-    function removeStopwordsFromString(str, stopwords) {
-      let words = str.split(" ");
-      words = words.filter((/** @type {any} */ word) => !stopwords.includes(word));
-      return words;
-    }
-
-    // @ts-ignore
-    function calculateTimeLeft(endTime) {
-      const currentTime = new Date();
-      // @ts-ignore
-      const diff = endTime - currentTime;
-
-      if (diff <= 0) {
-        return 'Time\'s up!';
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex].title < right[rightIndex].title) {
+        result.push(left[leftIndex]);
+        leftIndex++;
       } else {
-        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-        return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        result.push(right[rightIndex]);
+        rightIndex++;
       }
     }
 
-    async function fetchSearchResults() {
-      console.log("attempting search");
-      try {
-        const listingsCol = collection(db, 'listings');
-        // Stopword filtering
-        let searchQueryArray = removeStopwordsFromString(searchQuery, stopwords)
-        console.log(searchQueryArray);
-        /**
-          * @type {any[]}
-          */
-        let allResults = [];
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  }
 
-        // Search by term
-        let qSearchTerm = query(
-          listingsCol,
-          where('searchTerms', 'array-contains-any', searchQueryArray),
-          limit(itemsPerPage)
-        );
-        const querySnapshot = await getDocs(qSearchTerm);
-        const docsWithImages = querySnapshot.docs.map(doc => {
-          const data = doc.data();
-          return { ...data, imageUrl: data.imageURL }; // Access imageURL field directly
-        });
-        allResults = [...allResults, ...docsWithImages];
-        // Filter by category
-        if (category !== 'Any') {
-          allResults = allResults.filter(result => result.category === category);
-        }
-        // Remove duplicates
-        searchResults = Array.from(new Set(allResults.map(item => JSON.stringify(item)))).map(item => JSON.parse(item));
-        console.log(searchResults.length);
+  function removeStopwordsFromString(str, stopwords) {
+    let words = str.split(" ");
+    words = words.filter((word) => !stopwords.includes(word));
+    return words;
+  }
 
-        for (let i = 0; i < searchResults.length; i++) {
-          if (!searchResults[i].hasOwnProperty('title')) {
-            throw new Error('All objects must have a title property');
+  function calculateTimeLeft(time, id) {
+    let endDate = new Date(time.seconds * 1000 + time.nanoseconds / 1000000);
+    let currentTime = new Date();
+    let diff = endDate.getTime() - currentTime.getTime();
+
+    if (diff <= 0) {
+      return 'Listing Ended';
+    } else {
+      let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+      if (!$timers[id]) {
+        $timers[id] = setInterval(() => {
+          diff -= 1000;
+          if (diff <= 0) {
+            clearInterval($timers[id]);
+            delete $timers[id];
+          } else {
+            days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+            seconds = Math.floor((diff % (1000 * 60)) / 1000);
+            $timers[id] = `${days}d ${hours}h ${minutes}m ${seconds}s`;
           }
-        }
-        searchResults = mergeSort(searchResults);
-        return searchResults;
-      } catch (error) {
-        console.error('Error fetching search results:', error);
+        }, 1000);
       }
+      return $timers[id];
     }
+  }
 
 
+  async function fetchSearchResults() {
+    console.log("attempting search");
+    try {
+      const listingsCol = collection(db, 'listings');
+      // Stopword filtering
+      let searchQueryArray = removeStopwordsFromString(searchQuery, stopwords)
+      console.log(searchQueryArray);
+      let allResults = [];
 
-  
-    // @ts-ignore
-    function handleCategoryChange(event) {
-      category = event.target.value;
-      fetchSearchResults();
-    }
-  
-    function handleSearch() {
-      fetchSearchResults();
-    }
-  
-    // @ts-ignore
-    function handlePageChange(page) {
-      currentPage = page;
-      fetchSearchResults();
-    }
+      // Search by term
+      let qSearchTerm = query(
+        listingsCol,
+        where('searchTerms', 'array-contains-any', searchQueryArray),
+        limit(itemsPerPage)
+      );
+      const querySnapshot = await getDocs(qSearchTerm);
+      const docsWithImages = querySnapshot.docs.map(doc => {
+        const data = doc.data();
+        return { ...data, imageUrl: data.imageURL };
+      });
+      allResults = [...allResults, ...docsWithImages];
+      // Filter by category
+      if (category !== 'Any') {
+        allResults = allResults.filter(result => result.category === category);
+      }
+      // Filter by currency
+      allResults = allResults.filter(result => result.currency === displayCurrency);
+      // Remove duplicates
+      searchResults = Array.from(new Set(allResults.map(item => JSON.stringify(item)))).map(item => JSON.parse(item));
+      console.log(searchResults.length);
 
-    // @ts-ignore
-    function handleKeyDown(event) {
-        if (event.key === 'Enter' && searchQuery !== '') {
-            fetchSearchResults();
+      for (let i = 0; i < searchResults.length; i++) {
+        $timers[[searchResults[i].id]] = calculateTimeLeft(searchResults[i].end, searchResults[i].id);
+        if (!searchResults[i].hasOwnProperty('title')) {
+          throw new Error('All objects must have a title property');
         }
+      }
+      
+      searchResults = mergeSort(searchResults);
+      return searchResults;
+    } catch (error) {
+      console.error('Error fetching search results:', error);
     }
+  }
 
-  
-    onMount(fetchSearchResults);
+  function handleCategoryChange(event) {
+    category = event.target.value;
+    fetchSearchResults();
+  }
+
+  function handleCurrencyChange(event) {
+    displayCurrency = event.target.value;
+    if(displayCurrency === "GBP"){
+      currencySymbol = "£";
+    } else if (displayCurrency === "EUR") {
+      currencySymbol = "€"
+    } else if (displayCurrency === "USD") {
+      currencySymbol = "$";
+    } else if (displayCurrency === "JPY") {
+      currencySymbol = "¥";
+    }
+    fetchSearchResults();
+  }
+
+  function handleSearch() {
+    fetchSearchResults();
+  }
+
+  function handlePageChange(page) {
+    currentPage = page;
+    fetchSearchResults();
+  }
+
+  function handleKeyDown(event) {
+      if (event.key === 'Enter' && searchQuery !== '') {
+          fetchSearchResults();
+      }
+  }
+
+
+  onMount(fetchSearchResults);
 </script>
-  
-  
+
+
 <div class="min-h-screen bg-gray-100 py-4 px-4 sm:px-6 lg:px-8">
-  <div class="container mx-auto flex justify-center space-x-4">
-    <input type="text" bind:value={searchQuery} placeholder="Search..." class="w-1/2 px-2 py-1.5 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" on:keydown={handleKeyDown} />
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" on:click={handleSearch}>Search</button>
-    <!-- Category dropdown -->
-    <select id="category" bind:value={category} on:change={handleCategoryChange} class="w-1/16 shadow appearance-none border rounded py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        <option value="Any">Any</option>
-        <option value="Antiquity">Antiquities & Collectibles</option>
-        <option value="Baby">Baby & Kids</option>
-        <option value="Books">Books & Magazines</option>
-        <option value="Discs">DvDs & CDs</option>
-        <option value="Fashion">Fashion</option>
-        <option value="Health">Health & Beauty</option>
-        <option value="Home">Home & Furniture</option>
-        <option value="Music">Music & Instruments</option>
-        <option value="Office">Office Supplies</option>
-        <option value="Sports">Sports & Outdoors</option>
-        <option value="Technology">Technology</option>
-        <option value="Toys">Toys & Games</option>
-        <option value="VideoGameConsoles">Video Game Consoles</option>
-        <option value="VideoGames">Video Game Discs & Cartridges</option>
-      </select> </div>
-  
+<div class="container mx-auto flex justify-center space-x-4">
+  <input type="text" bind:value={searchQuery} placeholder="Search..." class="w-1/2 px-2 py-1.5 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" on:keydown={handleKeyDown} />
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" on:click={handleSearch}>Search</button>
+  <!-- Category dropdown -->
+  <select id="category" bind:value={category} on:change={handleCategoryChange} class="w-1/16 shadow appearance-none border rounded py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      <option value="Any">Any</option>
+      <option value="Antiquity">Antiquities & Collectibles</option>
+      <option value="Baby">Baby & Kids</option>
+      <option value="Books">Books & Magazines</option>
+      <option value="Discs">DvDs & CDs</option>
+      <option value="Fashion">Fashion</option>
+      <option value="Health">Health & Beauty</option>
+      <option value="Home">Home & Furniture</option>
+      <option value="Music">Music & Instruments</option>
+      <option value="Office">Office Supplies</option>
+      <option value="Sports">Sports & Outdoors</option>
+      <option value="Technology">Technology</option>
+      <option value="Toys">Toys & Games</option>
+      <option value="VideoGameConsoles">Video Game Consoles</option>
+      <option value="VideoGames">Video Game Discs & Cartridges</option>
+    </select> 
+    <select id="currency" bind:value={displayCurrency} on:change={handleCurrencyChange} class="w-1/16 shadow appearance-none border rounded py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      <option value="GBP">GBP</option>
+      <option value="EUR">EUR</option>
+      <option value="USD">USD</option>
+      <option value="JPY">JPY</option>
+    </select>
+  </div>
   <!-- Scrollable container for search results -->
-  <div class="overflow-auto max-h-60">
+  <div class="overflow-auto m-4 space-y-4">
     {#if searchResults.length > 0}
-      {#each searchResults as result (result.id)}
-      <div class="bg-gray-100 p-4 mb-2 rounded-md w-full flex justify-between items-center">
-        <div>
-          {result.title}
-          <!-- svelte-ignore a11y-img-redundant-alt -->
-          <img src="{result.imageUrl}" class="w-full h-auto rounded-md" alt="Listing image" />
-          <p>{result.description}</p>
+    {#each searchResults as result (result.id)}
+    <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+      <div class="flex justify-center">
+        <div class="flex justify-center">
+          <div class="flex-shrink-0">
+            <!-- svelte-ignore a11y-img-redundant-alt -->
+            <img class="h-40 w-40" src="{result.imageUrl}" alt="Listing image" />
+          </div>
+          <div class="ml-4">
+            <div class="text-lg leading-6 font-medium text-gray-900">{result.title}</div>
+            <p class="mt-2 text-sm text-gray-500">{result.description}</p>
+          </div>
         </div>
-        <div>
-          <p>Time Left: {calculateTimeLeft(new Date(result.end))}</p> 
-          <p>Current Price: {result.price}</p>
+        <div class="flex flex-col">
+          <div class="ml-4">
+            <dt class="text-m font-medium text-gray-500">Time Left:</dt>
+            <dd class="mt-1 text-m text-blue-700 font-bold">{$timers[result.id]}</dd>
+          </div>
+          <div class="ml-4">
+            <dt class="text-m font-medium text-gray-500">Current Price:</dt>
+            <dd class="mt-1 text-m font-bold text-blue-700">{currencySymbol}{parseFloat(result.price).toFixed(2)}</dd>
+          </div>
         </div>
       </div>
-      {/each}
+      
+    </div>
+    {/each}
     {:else}
-      <p class="text-gray-500">No results found.</p>
+    <p class="text-gray-500">No results found.</p>
     {/if}
-  </div>
-</div>
- 
+   </div>      
+ </div>
