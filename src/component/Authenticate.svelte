326ea673067ef89a3e011377403ac971a80c6ef9
Destystar <95242511+Destystar@users.noreply.github.com>
2024-02-29 21:46:58 +0000

    reverted username change

--- src/component/Authenticate.svelte
+++ src/component/Authenticate.svelte
@@ -100,16 +100,16 @@
                 const unsubscribe = onAuthStateChanged(auth, async (user) => {
                     if (user) {
                         console.log("after if");
-                        // updateProfile(user, { displayName: displayName });
-                        // await setDoc(doc(db, 'user', user.uid), {
-                        //         username: displayName,
-                        //         address: lineone,
-                        //         postcode: postcode,
-                        //         country: country,
-                        //     }, { merge: true });
-                        //     consolelog("successfully updated document");
-                        // console.log("update username:" + {displayName} + "to the user: " + user.uid);
-                        // await new Promise(resolve => setTimeout(resolve, 1000));
+                        updateProfile(user, { displayName: displayName });
+                        await setDoc(doc(db, 'user', user.uid), {
+                                username: displayName,
+                                address: lineone,
+                                postcode: postcode,
+                                country: country,
+                            }, { merge: true });
+                            consolelog("successfully updated document");
+                        console.log("update username:" + {displayName} + "to the user: " + user.uid);
+                        await new Promise(resolve => setTimeout(resolve, 1000));
                         console.log("before username");
                         // Check if the username is already taken
                         const usernameQuerySnapshot = await getDocs(query(collection(db, 'user'), where('displayName', '==', displayName)));
